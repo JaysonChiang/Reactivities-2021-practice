5bb11459cb4f76ae59909840cb0d42ada6687fb2
Jayson Chiang <jayson.chiang@outlook.com>
2021-05-27 22:41:20 +0800

    End of section 10

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -4,17 +4,26 @@ using Persistence;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
+using FluentValidation;
+using Application.Core;
 
 namespace Application.Activities
 {
   public class Edit
   {
-    public class Command : IRequest
+    public class Command : IRequest<Result<Unit>>
     {
       public Activity Activity { get; set; }
     }
+    public class CommandValidator : AbstractValidator<Command>
+    {
+      public CommandValidator()
+      {
+        RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
+      }
+    }
 
-    public class Handler : IRequestHandler<Command>
+    public class Handler : IRequestHandler<Command, Result<Unit>>
     {
       private readonly DataContext _context;
       private readonly IMapper _mapper;
@@ -23,15 +32,22 @@ namespace Application.Activities
         _context = context;
         _mapper = mapper;
       }
-      public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+      public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
       {
-          var activity = await _context.Activities.FindAsync(request.Activity.Id);
+        var activity = await _context.Activities.FindAsync(request.Activity.Id);
 
-          _mapper.Map(request.Activity, activity);
+        if (activity == null) return null;
 
-          await _context.SaveChangesAsync();
+        _mapper.Map(request.Activity, activity);
 
-          return Unit.Value;
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (!result)
+        {
+          return Result<Unit>.Failure("Failed to update activity");
+        }
+        
+        return Result<Unit>.Success(Unit.Value);
       }
     }
   }
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,11 +1,15 @@
 import { Container } from 'semantic-ui-react';
-import { Route, useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import NavBar from './NavBar';
 import ActivityDashbard from '../../features/activities/dashboard/ActivityDashboard';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import { observer } from 'mobx-react-lite';
 import HomePage from '../../features/home/HomePage';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
+import ServerError from '../../features/errors/ServerError';
 
 const App = () => {
   const location = useLocation();
@@ -17,15 +21,21 @@ const App = () => {
         path={'/(.+)'}
         render={() => (
           <>
+            <ToastContainer position="bottom-right" hideProgressBar />
             <NavBar />
             <Container style={{ marginTop: '7em' }}>
-              <Route exact path="/activities" component={ActivityDashbard} />
-              <Route path="/activities/:id" component={ActivityDetails} />
-              <Route
-                key={location.key}
-                path={['/createActivity', '/manage/:id']}
-                component={ActivityForm}
-              />
+              <Switch>
+                <Route exact path="/activities" component={ActivityDashbard} />
+                <Route path="/activities/:id" component={ActivityDetails} />
+                <Route
+                  key={location.key}
+                  path={['/createActivity', '/manage/:id']}
+                  component={ActivityForm}
+                />
+                <Route path="/errors" component={TestErrors} />
+                <Route path="/server-error" component={ServerError} />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </>
         )}
